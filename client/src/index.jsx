9e9aaa35e953e20bf106a8734e6c63facaa5d778
Giovaniavs <gavs2@cin.ufpe.br>
2020-07-06 00:51:47 -0300

    searchBar completed

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,32 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header/Header';
-import Menu from './components/Menu/Menu';
 import Footer from './components/Footer/Footer';
 import "./index.css"
 import Cardapio from './components/Cardapio/Cardapio';
 import axios from 'axios'
+import './components/Menu/Menu.css'
+
 
 function App() {
   const [produtos, setProdutos] = useState([]);
-
+  const [search, setSearch] = useState("");
   const loadProdutos = async () => {
     const res = await axios.get('http://localhost:3001/produtos/list');
     setProdutos(res.data);
   };
 
+  const cardapioFiltrado = produtos.filter( produto =>{
+    return produto.name.toLowerCase().includes( search.toLocaleLowerCase() )
+  });
+  
+  let itensCardapio = cardapioFiltrado.map((element) => {
+    return <Cardapio img_src={element.img.url} nome={element.name} desc={element.desc} preco={element.price} key={element._id}/>
+  });
+  const filtrarItensCardapio = (e) => {
+    setSearch(e.target.value)
+  }
+
   useEffect(() => {
     loadProdutos();
   }, []);
-
-  const itensCardapio = produtos.map((element) => {
-      return <Cardapio img_src={element.img.url} nome={element.name} desc={element.desc} preco={element.price} />
-    }
-  );
   return (
     <div className="app">
       <Header />
-      <Menu />
+      <div className="divMenu">
+            <h1 className= "cardapioStyle">
+                Cardápio
+            </h1>
+            <input id = "idSearchBar" className="searchBar" type="Search" onChange={filtrarItensCardapio}/>
+        </div>
       {itensCardapio}
       <Footer />
     </div>
